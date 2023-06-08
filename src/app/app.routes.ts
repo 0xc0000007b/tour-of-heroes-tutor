0af00a24ace6012ff821b0b0d57382a190f29688
0xc0000007b <rammleid123@gmail.com>
2023-06-08 14:03:26 +0300

    craeting ended

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./components/heroes/dashboard/dashboard.component').then(
+        (c) => c.DashboardComponent
+      ),
+  },
+  {
+    path: 'edit/:id',
+    loadComponent: () =>
+      import('./components/heroes/hero-editor/hero-editor.component').then(
+        (c) => c.HeroEditorComponent
+      ),
+  },
+  {
+    path: 'create-hero',
+    loadComponent: () =>
+      import('./components/heroes/hero-creator/hero-creator.component').then(
+        (c) => c.HeroCreatorComponent
+      ),
+  },
+];
